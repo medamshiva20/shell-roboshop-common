ae38f8c03975a8af3805810202243c2f095fbf97
medamshiva20 <medamshiva20@gmail.com>
2026-02-03 21:45:19 +0530

    mongodb configuration

--- mongodb.sh
+++ mongodb.sh
@@ -1,16 +1,17 @@
-#!/bin/bash 
+#!/bin/bash
 
 source ./common.sh
 
+
 check_root
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo 
-VALIDATE $? "Copying mongo repo"
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying Mongo Repo" 
 
 dnf install mongodb-org -y &>>$LOGS_FILE
-VALIDATE $? "Installing MongoDB Server"
+VALIDATE $? "Installing MongoDB server"
 
-systemctl enable mongod
+systemctl enable mongod &>>$LOGS_FILE
 VALIDATE $? "Enable MongoDB"
 
 systemctl start mongod
@@ -20,7 +21,6 @@ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections"
 
 systemctl restart mongod
-VALIDATE "Restarted MongoDB"
-
+VALIDATE $? "Restarted MongoDB"
 
 print_total_time
