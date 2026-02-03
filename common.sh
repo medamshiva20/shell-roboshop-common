fb62b7d314c054cfc49cf5dc768a3abd807fa545
medamshiva20 <medamshiva20@gmail.com>
2026-02-03 16:38:13 +0530

    mongodb configuration

--- common.sh
+++ common.sh
@@ -1,38 +1,39 @@
-#!/bin/bash
+    #!/bin/bash
 
-USERID=$(id -u)
-LOGS_DIR="/var/log/shell-roboshop"
-LOG_FILE="$LOGS_DIR/$0.log"
-START_TIME=$(date +%s)
+    USERID=$(id -u)
+    LOGS_FOLDER="/var/log/shell-roboshop"
+    LOGS_FILE="$LOGS_FOLDER/$0.log"
+    R="\e[31m"
+    G="\e[32m"
+    Y="\e[33m"
+    N="\e[0m"
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-mkdir -p $LOGS_DIR
-
-echo  "$(date "%Y-%m-%d %H:%M:%S) | Script started executed at:$(date)" | tee -a $LOG_FILE
-
-check_root(){
-if [ $USERID -ne 0 ] ;
-then 
-    echo -e "$R Please run this script with root user $N" | tee -a $LOG_FILE
-    exit 1
-fi
-}
+    START_TIME=$(date +%s)
 
 
-VALIDATE(){
-    if [ $1 -ne 0 ] ;
-    then 
-        echo -e "$2...$R FAILURE $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2...$G SUCCESS $N" | tee -a $LOG_FILE
-    fi
+    mkdir -p $LOGS_FOLDER
+
+    echo "$(date "+%Y-%m-%d %H:%M:%S") | Script started executing at: $(date)" | tee -a $LOGS_FILE
+
+    check_root(){
+        if [ $USERID -ne 0 ]; then
+            echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+            exit 1
+        fi
+    }
+
+    VALIDATE(){
+        if [ $1 -ne 0 ]; then
+            echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+            exit 1
+        else
+            echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+        fi
+    }
+
 
     print_total_time(){
         END_TIME=$(date +%s)
-        TOTAL_TIME=$(( $ENDTIME - $START_TIME ))
-        echo -e "$(date +%Y-%m-%d %H:%M:%S) | Script executed in : $G $TOTAL_TIME $N " | tee -a $LOG_FILE
+        TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+        echo -e "$(date "+%Y-%m-%d %H:%M:%S") | Script execute in: $G $TOTAL_TIME seconds $N" | tee -a $LOGS_FILE
     }
