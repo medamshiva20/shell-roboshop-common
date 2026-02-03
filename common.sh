99e6aee46fb9c2ed82b1173d78038b1f8449ff67
medamshiva20 <medamshiva20@gmail.com>
2026-02-03 21:52:00 +0530

    catalogue configuration

--- common.sh
+++ common.sh
@@ -1,39 +1,36 @@
-    #!/bin/bash
+#!/bin/bash
 
-    USERID=$(id -u)
-    LOGS_FOLDER="/var/log/shell-roboshop"
-    LOGS_FILE="$LOGS_FOLDER/$0.log"
-    MONGODB_HOST=mongodb.sivadevops.site
-    #YSQL_HOST=mysql.sivadevops.site
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+START_TIME=$(date +%s)
+MONGODB_HOST=mongodb.sivadevops.site
+MYSQL_HOST=mysql.sivadevops.site
 
-    SCRIPT_DIR=$PWD
-    R="\e[31m"
-    G="\e[32m"
-    Y="\e[33m"
-    N="\e[0m"
+mkdir -p $LOGS_FOLDER
 
-    START_TIME=$(date +%s)
+echo "$(date "+%Y-%m-%d %H:%M:%S") | Script started executing at: $(date)" | tee -a $LOGS_FILE
 
+check_root(){
+    if [ $USERID -ne 0 ]; then
+        echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+        exit 1
+    fi
+}
 
-    mkdir -p $LOGS_FOLDER
-
-    echo "$(date "+%Y-%m-%d %H:%M:%S") | Script started executing at: $(date)" | tee -a $LOGS_FILE
-
-    check_root(){
-        if [ $USERID -ne 0 ]; then
-            echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
-            exit 1
-        fi
-    }
-
-    VALIDATE(){
-        if [ $1 -ne 0 ]; then
-            echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
-            exit 1
-        else
-            echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
-        fi
-    }
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
 
 nodejs_setup(){
     dnf module disable nodejs -y &>>$LOGS_FILE
@@ -45,25 +42,34 @@ nodejs_setup(){
     dnf install nodejs -y &>>$LOGS_FILE
     VALIDATE $? "Install NodeJS"
 
-     npm install  &>>$LOGS_FILE
+    npm install  &>>$LOGS_FILE
     VALIDATE $? "Installing dependencies"
 }
 
-java_setup() {
-        dnf install maven -y &>>$LOGS_FILE
-        VALIDATE $? "Installing Maven"
+java_setup(){
+    dnf install maven -y &>>$LOGS_FILE
+    VALIDATE $? "Installing Maven"
 
-        cd /app 
-        mvn clean package &>>$LOGS_FILE
-        VALIDATE $? "Installing and Building $app_name"
+    cd /app 
+    mvn clean package &>>$LOGS_FILE
+    VALIDATE $? "Installing and Building $app_name"
 
-        mv target/$app_name-1.0.jar $app_name.jar 
-        VALIDATE $? "Moving and Renaming $app_name"
+    mv target/$app_name-1.0.jar $app_name.jar 
+    VALIDATE $? "Moving and Renaming $app_name"
 }
 
+python_setup(){
+    dnf install python3 gcc python3-devel -y &>>$LOGS_FILE
+    VALIDATE $? "Installing Python"
+
+    cd /app 
+    pip3 install -r requirements.txt &>>$LOGS_FILE
+    VALIDATE $? "Installing dependencies"
+}
 
 app_setup(){
-        id roboshop &>>$LOGS_FILE
+    # creating system user
+    id roboshop &>>$LOGS_FILE
     if [ $? -ne 0 ]; then
         useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
         VALIDATE $? "Creating system user"
@@ -71,31 +77,31 @@ app_setup(){
         echo -e "Roboshop user already exist ... $Y SKIPPING $N"
     fi
 
-        mkdir -p /app 
-        VALIDATE $? "Creating app directory"
+    # downloading the app
+    mkdir -p /app 
+    VALIDATE $? "Creating app directory"
 
-        curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>LOGS_FILE
-        VALIDATE $? "Downloading catalogue code"
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip  &>>$LOGS_FILE
+    VALIDATE $? "Downloading $app_name code"
 
-        cd /app
-        VALIDATE $? "Moving to app directory"
+    cd /app
+    VALIDATE $? "Moving to app directory"
 
-        rm -rf /app/*
-        VALIDATE $? "Removing existing code"
+    rm -rf /app/*
+    VALIDATE $? "Removing existing code"
 
-        unzip /tmp/$app_name.zip &>>$LOGS_FILE
-        VALIDATE $? "Uzip $app_name code"
+    unzip /tmp/$app_name.zip &>>$LOGS_FILE
+    VALIDATE $? "Uzip $app_name code"
 }
+
 systemd_setup(){
-
-
     cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
     VALIDATE $? "Created systemctl service"
 
     systemctl daemon-reload
-    systemctl enable $app_name &>>$LOGS_FILE
+    systemctl enable $app_name  &>>$LOGS_FILE
     systemctl start $app_name
-    VALIDATE $? "Starting and enabling catalogue"
+    VALIDATE $? "Starting and enabling $app_name"
 }
 
 app_restart(){
@@ -103,8 +109,8 @@ app_restart(){
     VALIDATE $? "Restarting $app_name"
 }
 
-    print_total_time(){
-        END_TIME=$(date +%s)
-        TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-        echo -e "$(date "+%Y-%m-%d %H:%M:%S") | Script execute in: $G $TOTAL_TIME seconds $N" | tee -a $LOGS_FILE
-    }
+print_total_time(){
+    END_TIME=$(date +%s)
+    TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | Script execute in: $G $TOTAL_TIME seconds $N" | tee -a $LOGS_FILE
+}
