4820cb68d120af88faee42bc3cef69e8a2904d80
medamshiva20 <medamshiva20@gmail.com>
2026-02-03 22:16:57 +0530

    shipping configuration

--- shipping.sh
+++ shipping.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 source ./common.sh
 app_name=shipping
@@ -11,13 +11,12 @@ systemd_setup
 dnf install mysql -y  &>>$LOGS_FILE
 VALIDATE $? "Installing MySQL"
 
-mysql -h 10.1.1.243 -pRoboShop@1 -e 'use cities'
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
 if [ $? -ne 0 ]; then
 
-mysql -h 10.1.1.243 -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
-mysql -h 10.1.1.243 -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
-mysql -h 10.1.1.243 -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
-
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
     VALIDATE $? "Loaded data into MySQL"
 else
     echo -e "data is already loaded ... $Y SKIPPING $N"
